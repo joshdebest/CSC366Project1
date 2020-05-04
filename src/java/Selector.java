3be009e77553254b4c63d92c46c643fc4e5bb4f2
goodellalex5 <goodellalex5@gmail.com>
2020-05-04 12:52:11 -0700

    added check in functionality

--- src/java/Selector.java
+++ src/java/Selector.java
@@ -109,7 +109,7 @@ public class Selector implements Serializable {
             case "Check Out a Customer":
                 return "checkOut";
             case "View Room Prices":
-                return "roomprices";
+                return "SelectDate";
             default:
                 return null;
         }
